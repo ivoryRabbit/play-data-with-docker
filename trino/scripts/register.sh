4f2c203ed98760cf044fec6bddd629d884a4ff8c
ivoryRabbit <tsi00008@gmail.com>
2024-01-14 17:18:18 +0900

    imp: add trino workers and seperate them for each coordinator

---
+++ trino/scripts/register.sh
@@ -0,0 +1,2 @@
+curl -H "Content-Type: application/json" -X POST localhost:9080/gateway/backend/modify/update -d @scripts/register-trino-1.json
+curl -H "Content-Type: application/json" -X POST localhost:9080/gateway/backend/modify/update -d @scripts/register-trino-2.json
