5d7b1750cf53206759cb65fdc59176f61cb56a6a
ivoryRabbit <tsi00008@gmail.com>
2024-04-14 19:55:48 +0900

    imp: seperate out table api example job

--- flink/consumer/src/main/scala/org/example/Job.scala
+++ flink/consumer/src/main/scala/org/example/Job.scala
@@ -1,13 +1,12 @@
 package org.example
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy
-import org.apache.flink.api.common.serialization.{DeserializationSchema, SimpleStringSchema}
+import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.connector.base.DeliveryGuarantee
 import org.apache.flink.connector.kafka.sink.{KafkaRecordSerializationSchema, KafkaSink}
 import org.apache.flink.connector.kafka.source.KafkaSource
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
-import org.apache.flink.table.api.bridge.java.StreamTableEnvironment
 
 
 object Job {
@@ -43,47 +42,5 @@ object Job {
     streamLines.sinkTo(kafkaSink)
 
     env.execute("Flink Streaming Scala Example")
-  }
-}
-
-object Job {
-  def main(args: Array[String]): Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
-
-     tableEnv.executeSql(
-       """
-         | CREATE TABLE ratings (
-         |   user_id INT
-         |   movie_id INT
-         |   rating FLOAT
-         |   timestamp LONG
-         | ) WITH (
-         |   'connector' = 'kafka',
-         |   'topic' = 'input.flink.dev',
-         |   'properties.bootstrap.servers' = 'kafka:9094',
-         |   'format' = 'json'
-         | )
-         |""".stripMargin
-     )
-
-     tableEnv.executeSql(
-       """
-         | CREATE TABLE populars (
-         |   movie_id INT
-         |   rating FLOAT
-         | ) WITH (
-         |   'connector' = 'kafka',
-         |   'topic' = 'output.flink.dev',
-         |   'properties.bootstrap.servers' = 'kafka:9094',
-         |   'format' = 'json'
-         | )
-         |""".stripMargin
-     )
-
-     tableEnv
-       .sqlQuery("SELECT movie_id, avg(rating) AS word FROM ratings GROUP BY movie_id")
-       .executeInsert("populars")
-       .wait()
   }
 }
---
+++ flink/consumer/src/main/scala/org/example/TableAPIJob.scala
@@ -0,0 +1,57 @@
+package org.example
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment
+
+
+object TableAPIJob {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
+
+    tableEnv.executeSql(
+      """
+        | CREATE TABLE ratings (
+        |   user_id INT,
+        |   movie_id INT,
+        |   rating FLOAT,
+        |   `timestamp` BIGINT
+        | ) WITH (
+        |   'connector' = 'kafka',
+        |   'topic' = 'input.flink.dev',
+        |   'properties.bootstrap.servers' = 'kafka:9092',
+        |   'properties.group.id' = 'flink',
+        |   'properties.auto.offset.reset' = 'latest',
+        |   'format' = 'json'
+        | )
+        |""".stripMargin
+    )
+
+    tableEnv.executeSql(
+      """
+        | CREATE TABLE ratings_clean (
+        |   user_id INT,
+        |   movie_id INT,
+        |   rating FLOAT,
+        |   created_at STRING
+        | ) WITH (
+        |   'connector' = 'kafka',
+        |   'topic' = 'output.flink.dev',
+        |   'properties.bootstrap.servers' = 'kafka:9092',
+        |   'format' = 'json'
+        | )
+        |""".stripMargin
+    )
+
+    tableEnv
+      .executeSql(
+        """
+          | INSERT INTO ratings_clean
+          | SELECT user_id, movie_id, rating, FROM_UNIXTIME(`timestamp`) AS created_at
+          | FROM ratings
+          |""".stripMargin)
+//      .sqlQuery("SELECT user_id, movie_id, rating, TO_TIMESTAMP_LTZ(`timestamp`, 0) AS created_at FROM ratings")
+//      .executeInsert("ratings_clean")
+//      .wait()
+  }
+}
