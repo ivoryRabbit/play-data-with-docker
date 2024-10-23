a6b258d4039bcd6b4e747dd5bf91dfcf2f9a6409
ivoryRabbit <tsi00008@gmail.com>
2024-10-24 00:07:41 +0900

    feat: add airbyte on k8s

--- airbyte/bin/run.sh
+++ airbyte/bin/run.sh
@@ -7,3 +7,12 @@ helm install airbyte-local airbyte/airbyte
 ## helm install --values ../values.yaml airbyte-local airbyte/airbyte
 
 ## kubectl apply -f airbyte-logs-secrets.yaml -n <NAMESPACE>
+
+
+# kubectl --namespace airbyte port-forward airbyte-local-webapp-5b66d8c784-qwm2c 8080:8080
+
+# helm install airbyte-nginx oci://ghcr.io/nginxinc/charts/nginx-ingress --version 1.4.0 --namespace airbyte
+
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx --create-namespace
