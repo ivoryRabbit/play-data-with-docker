b5f6eb06c7efa2677f33a4b9ae85496928192b9f
ivoryRabbit <tsi00008@gmail.com>
2024-11-02 17:43:37 +0900

    imp: remove unnecessary commands

--- airbyte/bin/run.sh
+++ airbyte/bin/run.sh
@@ -1,18 +1,10 @@
-## brew install helm
-
 helm repo add airbyte https://airbytehq.github.io/helm-charts
+
 helm repo update
 
-helm install airbyte-local airbyte/airbyte
-## helm install --values ../values.yaml airbyte-local airbyte/airbyte
+kubectl create namespace airbyte
 
-## kubectl apply -f airbyte-logs-secrets.yaml -n <NAMESPACE>
+helm --namespace airbyte install airbyte-local airbyte/airbyte
+helm --namespace airbyte install airbyte-nginx oci://ghcr.io/nginxinc/charts/nginx-ingress
 
-
-# kubectl --namespace airbyte port-forward airbyte-local-webapp-5b66d8c784-qwm2c 8080:8080
-
-# helm install airbyte-nginx oci://ghcr.io/nginxinc/charts/nginx-ingress --version 1.4.0 --namespace airbyte
-
-helm upgrade --install ingress-nginx ingress-nginx \
-  --repo https://kubernetes.github.io/ingress-nginx \
-  --namespace ingress-nginx --create-namespace
+kubectl --namespace airbyte apply -f ingress/nginx.yaml
